85128899e59d78217bd36b52bf37824fb32ca96f
Jake Kealey <134609350+JakeKea@users.noreply.github.com>
2026-09-15 09:06:09 +0000

    add: Model that maps spec comm code to description

---
+++ models/reference/data_dictionary/pss_spec_comm.sql
@@ -0,0 +1,41 @@
+with cs as (
+    select
+        split_part(npo_c_category, ' - ', 1)  as spec_comm_code,
+        split_part(npo_c_category, ' - ', 2) as spec_comm_desc,
+        case
+                when file_name like '%201920%' then 2019
+                when file_name like '%202021%' then 2020
+                when file_name like '%202122%' then 2021
+                when file_name like '%202223%' then 2022
+                when file_name like '%202324%' then 2023
+                when file_name like '%202425%' then 2024
+                when file_name like '%202526%' then 2025
+                when file_name like '%202627%' then 2026
+                when file_name like '%202728%' then 2027
+                when file_name like '%202829%' then 2028
+                when file_name like '%202930%' then 2029
+                when file_name like '%203031%' then 2030
+                else  0
+        end as file_period
+    from {{ref('stg_ukhfd_pss_summary_of_id_code_sets')}}
+
+    where ps_flag not like '%_Exc%'
+
+    qualify 
+        file_period = max(file_period) over (partition by spec_comm_code)
+        and row_number() over (partition by spec_comm_code order by spec_comm_desc) = 1
+)
+
+select distinct
+    spec_comm_code,
+    spec_comm_desc,
+    case 
+        when file_period > 0 
+        then cast(file_period as string) || '/' || right(cast(file_period + 1 as string), 2) 
+        when file_period = 0 
+        then 'Prior to 2019-20'
+        else null 
+    end as source_period
+    
+from cs
+order by spec_comm_code
