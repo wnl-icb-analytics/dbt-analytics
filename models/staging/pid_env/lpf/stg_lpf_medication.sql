287f522eaad8c8b0909275e621e2939aec8e6d14
Emily Baldwin <emily.baldwin20@nhs.net>
2026-09-24 16:08:57 +0100

    Feat: cleaning the staging of medicines to be more usable - clear organisation, linkable, dedup not rereviewed. Needs further refinement but leaving columns whilst other flows don't come through

--- models/staging/pid_env/lpf/stg_lpf_medication.sql
+++ models/staging/pid_env/lpf/stg_lpf_medication.sql
@@ -19,4 +19,46 @@ deduplicated as (
         ) as rn
     from cleaned
 )
-select * exclude (rn) from deduplicated where rn = 1
+
+select  medication_id
+    , {{ clean_organisation_id("left(split_part(metadata_file_path, '/', 1), 5)") }} as organisation_id
+    -- encounter details
+    , encounter_id
+    , {{ consistent_sk_patient_id_format('person_id') }} as sk_patient_id
+    , prescribing_provider_id
+    , to_date(start_date) as start_date
+    , to_date(stop_date) as end_date
+    -- drug
+    , drug_code_id
+    , drug_code_system_id -- TO DO: request populated or add by provider case when e.g., RAL == CDC's NCHS discontinued system?
+    , drug_display
+    -- administration information
+    , dose_quantity_unit_code_id
+    , dose_quantity_unit_code_system_id
+    , dose_quantity_unit_display
+    , dose_volume_amount
+    , dose_volume_unit_code_id
+    , dose_volume_unit_code_system_id
+    , dose_volume_unit_display
+    , route_code_id
+    , route_code_system_id -- TO DO: request populated 
+    , route_display
+    -- order outcome
+    , order_status_code_id
+    , order_status_code_system_id
+    , order_status_display
+    , order_detail_line
+
+    -- Not currently populated administrative information kept to review as more providers are involved
+    , frequency_code_id
+    , frequency_code_system_id
+    , frequency_display
+    , dose_strength_amount
+    , dose_strength_unit_code_id
+    , dose_strength_unit_code_system_id
+    , dose_strength_unit_display
+
+
+from deduplicated where rn = 1
+
+
